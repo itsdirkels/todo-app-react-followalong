4764965edb3842e278f1a2e176a0335694e91bb9
Daniel Wessels <34720459+itsdirkels@users.noreply.github.com>
2024-04-19 12:34:23 +0200

    Bugs Fixed

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,7 +7,7 @@ export function TodoList( {todos, toggleTodo, deleteTodo} ) {
         <ul className="list">
         {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
-            <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+            return <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
         })}
         </ul>
     );
